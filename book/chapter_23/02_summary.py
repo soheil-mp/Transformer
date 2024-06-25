1ea8a3f08d09ed182f6ba312dc1b0f3db8103cb1
Soheil <Soheil.mpg@gmail.com>
2024-06-25 13:11:14 +0200

    re-arrange repo

--- book/chapter_23/02_summary.py
+++
@@ -1,5 +0,0 @@
-from summarizer import Summarizer
-text = open("article.txt").read()
-model = Summarizer('distilbert-base-uncased')
-result = model(text, num_sentences=3)
-print(result)
--- book/chapter_23/03_answering.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-text = open("article.txt").read()
-question = "What is BOE doing?"
-
-answering = pipeline("question-answering",
-                     model='distilbert-base-uncased-distilled-squad')
-result = answering(question=question, context=text)
-print(result)
